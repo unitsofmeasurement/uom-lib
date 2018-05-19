e592e9287489d739ea7966845970f4c8b48aee90
Werner Keil <werner.keil@gmx.net>
2018-05-20 01:05:31 +0200

    35: Create Jackson JSON module
    Task-Url: https://github.com/unitsofmeasurement/uom-lib/issues/issues/35

--- jackson/src/main/java/tech/uom/lib/jackson/UnitJacksonModule.java
+++ jackson/src/main/java/tech/uom/lib/jackson/UnitJacksonModule.java
@@ -1,3 +1,32 @@
+/*
+ *  Units of Measurement Jackson Library for JSON support
+ *  Copyright (c) 2012-2018, Werner Keil and others
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of JSR-363, Units of Measurement nor the names of their contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package tech.uom.lib.jackson;
 
 import java.io.IOException;
@@ -30,14 +59,20 @@ public class UnitJacksonModule extends SimpleModule {
 
 	public UnitJacksonModule() {
         super("UnitJsonSerializationModule", new Version(1, 3, 3, null, 
-        		UnitJacksonModule.class.getPackage().getName(), "jackson-module-unitsofmeasure"));
+        		UnitJacksonModule.class.getPackage().getName(), "uom-lib-jackson"));
 
         addSerializer(Unit.class, new UnitJsonSerializer());
         addDeserializer(Unit.class, new UnitJsonDeserializer());
     }
 
-    private class UnitJsonSerializer extends StdScalarSerializer<Unit> {
-        protected UnitJsonSerializer() {
+	@SuppressWarnings("rawtypes")
+	private class UnitJsonSerializer extends StdScalarSerializer<Unit> {
+        /**
+		 * 
+		 */
+		private static final long serialVersionUID = 2500234678114311932L;
+
+		protected UnitJsonSerializer() {
             super(Unit.class);
         }
 
@@ -57,7 +92,8 @@ public class UnitJacksonModule extends SimpleModule {
         }
     }
 
-    private class UnitJsonDeserializer extends StdScalarDeserializer<Unit> {
+    @SuppressWarnings("rawtypes")
+	private class UnitJsonDeserializer extends StdScalarDeserializer<Unit> {
         /**
 		 * 
 		 */
@@ -74,7 +110,7 @@ public class UnitJacksonModule extends SimpleModule {
             if (currentToken == JsonToken.VALUE_STRING) {
                 return UCUMFormat.getInstance(Variant.CASE_INSENSITIVE).parse(jsonParser.getText(), new ParsePosition(0));
             }
-            throw deserializationContext.wrongTokenException(jsonParser,
+            throw deserializationContext.wrongTokenException(jsonParser, String.class,
                     JsonToken.VALUE_STRING,
                     "Expected unit value in String format");
         }
---
+++ jackson/src/test/java/tech/uom/lib/jackson/TestUnitJacksonModule.java
@@ -0,0 +1,155 @@
+/*
+ *  Units of Measurement Jackson Library for JSON support
+ *  Copyright (c) 2012-2018, Werner Keil and others
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of JSR-363, Units of Measurement nor the names of their contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package tech.uom.lib.jackson;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import tech.uom.lib.jackson.UnitJacksonModule;
+import systems.uom.ucum.UCUM;
+import tec.units.indriya.unit.Units;
+
+import javax.measure.Unit;
+
+import static tec.units.indriya.AbstractUnit.ONE;
+import static tec.units.indriya.unit.MetricPrefix.KILO;
+import static tec.units.indriya.unit.MetricPrefix.MILLI;
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for UnitJacksonModule
+ */
+public class TestUnitJacksonModule {
+	// can't directly unit test the Jackson Module classes; need to go through
+	// JsonFactory
+	private JsonFactory jsonFactory;
+
+	@Before
+	public void setUp() throws Exception {
+		final ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new UnitJacksonModule());
+		this.jsonFactory = new JsonFactory(mapper);
+	}
+
+	@Test
+	public void testSerializeArea() throws Exception {
+		assertEquals("Expected JSON with a UCUM representation of the area unit", "\"m2\"",
+				serialize(Units.SQUARE_METRE));
+	}
+
+	@Test
+	public void testSerializeTemperature() throws Exception {
+		assertEquals("Expected JSON with a UCUM representation of the temperature unit", "\"[degF]\"",
+				serialize(UCUM.FAHRENHEIT));
+		assertEquals("Expected JSON with a UCUM representation of the temperature unit", "\"Cel\"",
+				serialize(Units.CELSIUS));
+	}
+
+	@Test
+	public void testSerializeLength() throws Exception {
+		assertEquals("Expected JSON with a UCUM representation of the length unit", "\"[mi_i]\"",
+				serialize(UCUM.MILE_INTERNATIONAL));
+		assertEquals("Expected JSON with a UCUM representation of the length unit", "\"m\"", serialize(Units.METRE));
+		assertEquals("Expected JSON with a UCUM representation of the length unit", "\"km\"",
+				serialize(KILO(UCUM.METER)));
+		assertEquals("Expected JSON with a UCUM representation of the length unit", "\"mm\"",
+				serialize(MILLI(UCUM.METER)));
+	}
+
+	@Test
+	public void testSerializeSpeed() throws Exception {
+		assertEquals("Expected JSON with a UCUM representation of the speed unit", "\"[kph]\"",
+				serialize(Units.KILOMETRE_PER_HOUR));
+	}
+
+	@Test
+	public void testParseArea() throws Exception {
+		final Unit<?> parsedUnit = parse("\"[sft_i]\"", Unit.class);
+		assertEquals("The Unit<Area> in the parsed JSON doesn't match", UCUM.SQUARE_FOOT_INTERNATIONAL, parsedUnit);
+	}
+
+	@Test
+	public void testParseTemperature() throws Exception {
+		final Unit<?> parsedUnit = parse("\"Cel\"", Unit.class);
+		assertEquals("The Unit<Temperature> in the parsed JSON doesn't match", Units.CELSIUS, parsedUnit);
+	}
+
+	@Test
+	public void testParseTemperatureInverse() throws Exception {
+		final Unit<?> parsedUnit = parse("\"1/K\"", Unit.class);
+		assertEquals("The Unit<Temperature> in the parsed JSON doesn't match", ONE.divide(Units.KELVIN), parsedUnit);
+	}
+
+	@Test
+	public void testParseLength() throws Exception {
+		Unit<?> parsedUnit = parse("\"m\"", Unit.class);
+		assertEquals("The Unit<Length> in the parsed JSON doesn't match", Units.METRE, parsedUnit);
+	}
+
+	@Test(expected = JsonParseException.class)
+	public void testParseWithUnrecognizedField() throws Exception {
+		parse("foobar", Unit.class);
+	}
+
+	@Test
+	@Ignore("solve km formatting") // TODO solve km parsing
+	public void testParseLengthKm() throws Exception {
+		final Unit<?> parsedUnit = parse("\"km\"", Unit.class);
+		assertEquals("The Unit<Length> in the parsed JSON doesn't match", KILO(Units.METRE), parsedUnit);
+	}
+
+	protected String serialize(Object objectToSerialize) throws IOException {
+		final Writer writer = new StringWriter();
+		final JsonGenerator generator = this.jsonFactory.createJsonGenerator(writer);
+
+		generator.writeObject(objectToSerialize);
+		generator.close();
+		return writer.toString();
+	}
+
+	protected <T> T parse(String json, Class<T> aClass) throws IOException {
+		final JsonParser parser = this.jsonFactory.createJsonParser(json);
+		T object = parser.readValueAs(aClass);
+
+		parser.close();
+		return object;
+	}
+}
