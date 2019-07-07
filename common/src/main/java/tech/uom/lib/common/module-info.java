29b8398318e8b2e480959bce45df95c738a0127b
Werner Keil <werner.keil@gmx.net>
2019-07-07 22:00:18 +0200

    198: Final deliverables
    Task-Url: https://github.com/unitsofmeasurement/unit-api/issues/issues/198

--- common/src/main/java/tech/uom/lib/common/module-info.java
+++ common/src/main/java/tech/uom/lib/common/module-info.java
@@ -1,7 +1,7 @@
 
 /*
  *  Units of Measurement Common Library for Java
- *  Copyright (c) 2005-2018, Jean-Marie Dautelle, Werner Keil and others.
+ *  Copyright (c) 2005-2019, Jean-Marie Dautelle, Werner Keil and others.
  *
  * All rights reserved.
  *
