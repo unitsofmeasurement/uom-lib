30ba5feecae3e6d440d4292554c9e657e3f51df7
Werner Keil <werner.keil@gmx.net>
2019-10-27 23:24:11 +0100

    49: Change package name
    Task-Url: https://github.com/unitsofmeasurement/uom-demos/issues/issues/49

--- jackson/src/main/java/tech/uom/lib/jackson/DimensionJsonDeserializer.java
+++ jackson/src/main/java/tech/uom/lib/jackson/DimensionJsonDeserializer.java
@@ -1,3 +1,32 @@
+/*
+ *  Units of Measurement Jackson Library for JSON support
+ *  Copyright (c) 2012-2019, Werner Keil and others
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of JSR-385, Units of Measurement nor the names of their contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package tech.uom.lib.jackson;
 
 import com.fasterxml.jackson.core.JsonParser;
@@ -9,11 +38,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 import javax.measure.Dimension;
-import tech.units.indriya.quantity.QuantityDimension;
+import tech.units.indriya.unit.UnitDimension;
 
 /**
- *
  * @author richter
+ * @author keilw
  */
 public class DimensionJsonDeserializer extends StdScalarDeserializer<Dimension> {
 
@@ -29,10 +58,11 @@ public class DimensionJsonDeserializer extends StdScalarDeserializer<Dimension> 
     @Override
     public Dimension deserialize(JsonParser p, DeserializationContext ctxt)
             throws IOException, JsonProcessingException {
-        Map<String, Integer> baseDimensionsStrings = p.readValueAs(Map.class);
+        @SuppressWarnings("unchecked")
+		Map<String, Integer> baseDimensionsStrings = p.readValueAs(Map.class);
         Map<Dimension, Integer> baseDimensions = new HashMap<>(baseDimensionsStrings.entrySet().stream()
                 .collect(Collectors.toMap(entry -> parseBaseDimension(entry.getKey()), entry -> entry.getValue())));
-        Dimension retValue = QuantityDimension.NONE;
+        Dimension retValue = UnitDimension.NONE;
         for (Dimension baseDimension : baseDimensions.keySet()) {
             int exp = baseDimensions.get(baseDimension);
             retValue = retValue.multiply(baseDimension.pow(exp));
@@ -43,19 +73,19 @@ public class DimensionJsonDeserializer extends StdScalarDeserializer<Dimension> 
     private static Dimension parseBaseDimension(String symbol) {
         switch (symbol) {
             case "[N]":
-                return QuantityDimension.AMOUNT_OF_SUBSTANCE;
+                return UnitDimension.AMOUNT_OF_SUBSTANCE;
             case "[I]":
-                return QuantityDimension.ELECTRIC_CURRENT;
+                return UnitDimension.ELECTRIC_CURRENT;
             case "[L]":
-                return QuantityDimension.LENGTH;
+                return UnitDimension.LENGTH;
             case "[J]":
-                return QuantityDimension.LUMINOUS_INTENSITY;
+                return UnitDimension.LUMINOUS_INTENSITY;
             case "[M]":
-                return QuantityDimension.MASS;
+                return UnitDimension.MASS;
             case "[\u0398]":
-                return QuantityDimension.TEMPERATURE;
+                return UnitDimension.TEMPERATURE;
             case "[T]":
-                return QuantityDimension.TIME;
+                return UnitDimension.TIME;
             default:
                 throw new IllegalArgumentException(String.format(
                         "dimension " + "symbol '%s' not supported, maybe dimensionless or " + "wrong universe?", symbol));
--- jackson/src/test/java/tech/uom/lib/jackson/DimensionJsonDeserializerTest.java
+++ jackson/src/test/java/tech/uom/lib/jackson/DimensionJsonDeserializerTest.java
@@ -1,3 +1,32 @@
+/*
+ *  Units of Measurement Jackson Library for JSON support
+ *  Copyright (c) 2012-2019, Werner Keil and others
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of JSR-385, Units of Measurement nor the names of their contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package tech.uom.lib.jackson;
 
 import com.fasterxml.jackson.core.JsonParser;
@@ -10,11 +39,11 @@ import java.nio.charset.StandardCharsets;
 import javax.measure.Dimension;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import tech.units.indriya.quantity.QuantityDimension;
+import tech.units.indriya.unit.UnitDimension;
 
 /**
- *
  * @author richter
+ * @author keilw
  */
 public class DimensionJsonDeserializerTest {
 
@@ -31,7 +60,7 @@ public class DimensionJsonDeserializerTest {
         JsonParser parser = objectMapper.getFactory().createParser(stream);
         DeserializationContext ctxt = objectMapper.getDeserializationContext();
         DimensionJsonDeserializer instance = new DimensionJsonDeserializer();
-        Dimension expResult = QuantityDimension.LENGTH.pow(2).multiply(QuantityDimension.TIME);
+        Dimension expResult = UnitDimension.LENGTH.pow(2).multiply(UnitDimension.TIME);
         Dimension result = instance.deserialize(parser, ctxt);
         assertEquals(expResult, result);
     }
